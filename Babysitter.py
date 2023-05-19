59f7afe7945f133302e39245d3bf2daf6a766684
avigailbendheim <hand1b3@mbjcomp.org.il>
2023-05-19 11:22:10 +0300

    1rr

---
+++ Babysitter.py
@@ -0,0 +1,12 @@
+class Babysitter:
+    def __init__(self, id, mail, firstname, lastname, year_of_birth, city, neighborhood, seminary, cell, experience):
+        self.id = id
+        self.mail = mail
+        self.firstname = firstname
+        self.lastname = lastname
+        self.year_of_birth = year_of_birth
+        self.city = city
+        self.neighborhood = neighborhood
+        self.seminary = seminary
+        self.cell = cell
+        self.experience = experience
---
+++ Family.py
@@ -0,0 +1,16 @@
+
+
+class Family:
+    def __init__(self, id, mail, firstname, lastname, city, neighborhood, cell, num_of_children, oldest_child, youngest_child):
+        self.id = id
+        self.mail = mail
+        self.firstname = firstname
+        self.lastname = lastname
+        self.city = city
+        self.neighborhood = neighborhood
+        self.cell = cell
+        self.num_of_children = num_of_children
+        self.oldest_children = oldest_child
+        self.youngest_children = youngest_child
+
+
